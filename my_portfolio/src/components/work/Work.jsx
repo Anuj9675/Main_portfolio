a8efd1e668d6a61b7135db985fc8069c9d24a6b8
Anuj Upadhyay <114591897+Anuj9675@users.noreply.github.com>
2023-11-25 11:29:59 +0530

    Update Work.jsx

--- my_portfolio/src/components/work/Work.jsx
+++ my_portfolio/src/components/work/Work.jsx
@@ -3,7 +3,7 @@ import WorkCard from "../../chip/WorkCard";
 
 const Work = () => {
   return (
-    <div id="works" className="container m-auto mt-16">
+    <div id="works" className="container m-auto mt-16 border">
       <div className="mb-5">
         <h3 className="text-5xl font-black text-gray-400 sm:text-2xl">Works</h3>
         <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
