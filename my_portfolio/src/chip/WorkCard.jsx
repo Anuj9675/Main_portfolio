867d7973d7c60e01ec832390332ee0fe6129b948
Anuj Upadhyay <114591897+Anuj9675@users.noreply.github.com>
2023-11-25 11:02:26 +0530

    Update WorkCard.jsx

--- my_portfolio/src/chip/WorkCard.jsx
+++ my_portfolio/src/chip/WorkCard.jsx
@@ -14,8 +14,15 @@ const WorkCard = () => {
           <div
             key={data.id}
             className="flex md:flex-col flex-row gap-24 md:gap-6 items-center"
+            style={{ flexDirection: 'row-reverse' }} // Reverse order for mobile view
           >
-            <div className="flex-1">
+            <div className="w-full md:w-[40%] md:order-2">
+              <div className="relative rounded-md overflow-hidden shadow-md">
+                <img src={data.img} alt={data.title} />
+              </div>
+            </div>
+
+            <div className="flex-1 md:w-full md:order-1">
               <div className="flex flex-col justify-between h-full">
                 <div>
                   <p className="text-gray-800 text-5xl md:text-3xl font-medium mb-4">
@@ -24,36 +31,27 @@ const WorkCard = () => {
                   <p className="text-gray-900 text-lg leading-[1.6] mb-5">
                     {data.desc}
                   </p>
-                  <div className="flex flex-col md:flex-row md:justify-between ">
-                  <div className="flex gap-4">
-                    <Link
-                      to={data.link}
-                      target="_blank"
-                      className="rounded-md shadow-md p-1 px-2 flex gap-2 items-center justify-center font-medium text-black"
-                    >
-                      <RxExternalLink className="bg-white rounded-full border w-[35px] h-[35px] p-2" />
-                      <p>View</p>
-                    </Link>
-                    <Link
-                      to={data.git}
-                      target="_blank"
-                      className="rounded-md shadow-md p-1 px-2 flex gap-2 items-center justify-center font-medium text-black"
-                    >
-                      <AiOutlineGithub className="bg-white rounded-full border w-[35px] h-[35px] p-2" />
-                      <p>Code</p>
-                    </Link>
+                  <div className="flex flex-col md:flex-row md:justify-between">
+                    <div className="flex gap-4">
+                      <Link
+                        to={data.link}
+                        target="_blank"
+                        className="rounded-md shadow-md p-1 px-2 flex gap-2 items-center justify-center font-medium text-black"
+                      >
+                        <RxExternalLink className="bg-white rounded-full border w-[35px] h-[35px] p-2" />
+                        <p>View</p>
+                      </Link>
+                      <Link
+                        to={data.git}
+                        target="_blank"
+                        className="rounded-md shadow-md p-1 px-2 flex gap-2 items-center justify-center font-medium text-black"
+                      >
+                        <AiOutlineGithub className="bg-white rounded-full border w-[35px] h-[35px] p-2" />
+                        <p>Code</p>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-                </div>
-                
-              </div>
-            </div>
-
-            <div className="w-[40%] md:w-[30%]">
-            <div className="relative rounded-md overflow-hidden shadow-md">
-                <img
-                  src={data.img}
-                />
               </div>
             </div>
           </div>
