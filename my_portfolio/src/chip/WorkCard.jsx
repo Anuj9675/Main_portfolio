65a5cdeb47adac03d88bc71d0abe3d0849496abc
Anuj Upadhyay <114591897+Anuj9675@users.noreply.github.com>
2023-11-25 11:04:39 +0530

    Update WorkCard.jsx

--- my_portfolio/src/chip/WorkCard.jsx
+++ my_portfolio/src/chip/WorkCard.jsx
@@ -9,32 +9,37 @@ const WorkCard = () => {
 
   return (
     <>
-      {reversedData.map((data) => {
+      {reversedData.map((item) => {
         return (
           <div
-            key={data.id}
-            className="flex md:flex-col flex-row gap-24 md:gap-6 items-center"
-            style={{ flexDirection: 'row-reverse' }} // Reverse order for mobile view
+            key={item.id}
+            className="flex gap-24 items-center"
+            style={{
+              flexDirection: 'row-reverse', // Reverses order for mobile view
+              '@media (max-width: 768px)': {
+                flexDirection: 'column', // Switches to column layout on smaller screens
+              },
+            }}
           >
-            <div className="w-full md:w-[40%] md:order-2">
+            <div className="w-full md:w-[40%]">
               <div className="relative rounded-md overflow-hidden shadow-md">
-                <img src={data.img} alt={data.title} />
+                <img src={item.img} alt={item.title} />
               </div>
             </div>
 
-            <div className="flex-1 md:w-full md:order-1">
+            <div className="flex-1">
               <div className="flex flex-col justify-between h-full">
                 <div>
                   <p className="text-gray-800 text-5xl md:text-3xl font-medium mb-4">
-                    {data.title}
+                    {item.title}
                   </p>
                   <p className="text-gray-900 text-lg leading-[1.6] mb-5">
-                    {data.desc}
+                    {item.desc}
                   </p>
                   <div className="flex flex-col md:flex-row md:justify-between">
                     <div className="flex gap-4">
                       <Link
-                        to={data.link}
+                        to={item.link}
                         target="_blank"
                         className="rounded-md shadow-md p-1 px-2 flex gap-2 items-center justify-center font-medium text-black"
                       >
@@ -42,7 +47,7 @@ const WorkCard = () => {
                         <p>View</p>
                       </Link>
                       <Link
-                        to={data.git}
+                        to={item.git}
                         target="_blank"
                         className="rounded-md shadow-md p-1 px-2 flex gap-2 items-center justify-center font-medium text-black"
                       >
