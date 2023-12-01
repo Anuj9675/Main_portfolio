802acab7f1c2d65212e10cba8fbf6178e1c45ca3
Anuj Upadhyay <114591897+Anuj9675@users.noreply.github.com>
2023-12-01 15:11:16 +0530

    Update WorkCard.jsx

--- my_portfolio/src/chip/WorkCard.jsx
+++ my_portfolio/src/chip/WorkCard.jsx
@@ -13,9 +13,9 @@ const WorkCard = () => {
         return (
           <div
             key={item.id}
-            className="flex flex-row md:flex-col items-center gap-24 md:gap-10 relative "
+            className="flex flex-row md:flex-col items-center gap-24 md:gap-10"
           >
-            <div className="absolute bottom-0 left-0 w-full flex justify-center gap-24 md:gap-8">
+            <div className="bottom-0 left-0 w-full flex justify-center gap-24 md:gap-8">
               <div className=" rounded-md overflow-hidden shadow-lg">
                 <img
                   src={item.img}
